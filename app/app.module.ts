ca0ace4d42d5ac9e06c9528e92c85afccfe33107
Robby Kim <robert.st.kim@gmail.com>
2016-11-02 16:49:20 -0400

    made app routing module its own file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 // looks like when you pass in modules and components into the NgModule
 // the rest of the app will have access to it
@@ -17,26 +18,7 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        // path /heroes opens HeroesComponent
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ]),
+    AppRoutingModule,
   ],
   // you put all your components in here
   declarations: [
