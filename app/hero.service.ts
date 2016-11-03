50eb9dd10fd1ca0095e1fc3d95113a28626495bb
Robby Kim <robert.st.kim@gmail.com>
2016-11-02 21:29:50 -0400

    added delete functionality

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -45,4 +45,13 @@ export class HeroService {
       .then(res => res.json().data)
       .catch(this.handleError);
   }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http
+      .delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 }
